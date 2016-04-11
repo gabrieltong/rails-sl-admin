bf70476752248b5c87cf5e99299e8d6973548a92
Gabriel Tong <tongyangmath@gmail.com>
2016-04-11 14:40:59 +0800

    上传图片

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,8 @@
 # encoding: UTF-8
 # user_type 1 代表病人， 2代表医生
 class ApiController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def create_mobile_file
     params[:mobile_file] = JSON.parse(params[:mobile_file]) if params[:mobile_file].is_a? String
     @mobile_file = MobileFile.new()
@@ -10,5 +12,18 @@ class ApiController < ApplicationController
     else
       render_fail
     end
+  end
+
+private
+  def person_params
+    params.require(:mobile_file).permit(:file)
+  end
+
+  def render_success(msg='')
+    render json: {:result=>1,:msg=>msg}
+  end
+
+  def render_fail(msg='')
+    render json: {:result=>0,:msg=>msg}
   end
 end
